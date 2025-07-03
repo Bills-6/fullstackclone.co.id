8d1b53c2272dd10c72a3287c752979ef32fb0765
Bills-6 <conutconut6@gmail.com>
2025-07-03 21:37:21 +0700

    fitur hamburger sudah ditambahkan

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+const navbar = document.querySelector(".navbar-collapse"),
+	ham = document.querySelector(".hamburger-bar");
 
+ham.addEventListener("click", () => {
+	navbar.classList.toggle("show");
+});
